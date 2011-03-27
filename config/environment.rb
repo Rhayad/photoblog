4b1c811103218c84c34d10556529c0d1e2242604
Marcus Kauth <traenen@rhayad.de>
2011-03-27 18:03:14 +0200

    Photblog

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,6 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the rails application
 Photoblog::Application.initialize!
+
+IMAGE_DIR = '/uploads'
+THUMBNAIL_DIR = IMAGE_DIR + '/thumbnails'
